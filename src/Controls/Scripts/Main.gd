7b46383e5829cea1ea4e88383b578ad0a6b05969
Haydara <haydaraal2005@gmail.com>
2022-09-24 09:32:34 -0700

    Added config window on new button press

--- src/Controls/Scripts/Main.gd
+++ src/Controls/Scripts/Main.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
+var configuration_window = preload("res://Controls/Scenes/ProjectConfig.tscn").instance()
 
+func _on_NewButton_pressed() -> void:
+	add_child(configuration_window)
 
 func _on_ExitButton_pressed() -> void:
 	get_tree().quit()
