e88d7c05a866b86e6cf646f5ca4c67a1876783c1
Haydara <haydaraal2005@gmail.com>
2022-09-25 16:53:41 -0700

    Moves the camera with the mouse cursor

--- src/Controls/Scripts/Main.gd
+++ src/Controls/Scripts/Main.gd
@@ -2,6 +2,9 @@ extends Node2D
 
 var configuration_window = preload("res://Controls/Scenes/ProjectConfig.tscn").instance()
 
+func _process(_delta: float) -> void:
+	$MouseCamera2D.offset = get_local_mouse_position() * 0.4
+
 func _on_NewButton_pressed() -> void:
 	add_child(configuration_window)
 
